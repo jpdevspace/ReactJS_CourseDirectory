971585cecfb8cd31291b49fcb827f73ba81650d2
jpwebdev <jjpwebdev@gmail.com>
2018-02-04 19:49:06 -0500

    Courses and Teachers Components added

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const About = () => (
-    <div className="main-content">
-      <h2>About</h2>
-      <p>The front end course directory lists many of the technologies, languages, libraries and frameworks I already know like HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
-    </div>
-  );
-  
-  export default About;
+const About = props => (
+  <div className="main-content">
+    <h2>{this.props.title}</h2>
+    <p>The front end course directory lists many of the technologies, languages, libraries and frameworks I already know like HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+  </div>
+);
+
+About.propTypes = {
+  title: PropTypes.string
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import About from './About';
-
+import Courses from './Courses';
+import Teachers from './Teachers';
 
 class App extends Component {
   render() {
@@ -14,7 +15,9 @@ class App extends Component {
         <div className="container">
           <Header />
           <Route exact path="/" component={Home} /> 
-          <Route path="/about" component={About} />
+          <Route path="/about" render={ () => <About title='About' /> } />
+          <Route path="/teachers" component={Teachers} />
+          <Route path="/courses" component={Courses} />
         </div>
       </BrowserRouter>
     );
